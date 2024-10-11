714d27795953bfab7ebd70fdc9751ed86934ddf3
Melinda Serven <melinda.serven@acquia.com>
2024-10-11 15:01:13 -0700

    How autowiring works

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,14 +16,13 @@ class MainController extends AbstractController
     public function homepage(
         StarshipRepository $starshipRepository,
         HttpClientInterface $client,
-        CacheInterface $cache
+        CacheInterface $issLocationPool
     ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $issData = $cache->get('iss_data', function(ItemInterface $item) use ($client): array {
-            $item->expiresAfter(5);
+        $issData = $issLocationPool->get('iss_data', function() use ($client): array {
             $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
 
             return $response->toArray();
