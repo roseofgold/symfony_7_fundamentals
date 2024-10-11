7f092e1300b578da57bbcbaf8fff68b6701180ce
Melinda Serven <melinda.serven@acquia.com>
2024-10-11 14:26:26 -0700

    The HTTPClient Service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,18 +6,26 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(StarshipRepository $starshipRepository): Response
+    public function homepage(
+        StarshipRepository $starshipRepository,
+        HttpClientInterface $client
+    ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
+        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+        $issData = $response->toArray();
+
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
             'ships' => $ships,
+            'issData' => $issData,
         ]);
     }
 }
