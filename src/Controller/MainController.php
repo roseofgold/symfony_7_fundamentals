6f932988a8185fe571c7617f388bfb29b93bfdd1
Melinda Serven <melinda.serven@acquia.com>
2024-10-11 14:48:39 -0700

    Cache Service and Cache Pools

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,8 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
@@ -13,14 +15,19 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function homepage(
         StarshipRepository $starshipRepository,
-        HttpClientInterface $client
+        HttpClientInterface $client,
+        CacheInterface $cache
     ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-        $issData = $response->toArray();
+        $issData = $cache->get('iss_data', function(ItemInterface $item) use ($client): array {
+            $item->expiresAfter(5);
+            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+            return $response->toArray();
+        });
 
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
